97df2831b734d5e78910201f981765d4deb5521c
Tarun <tarunpatidar742@gmail.com>
2023-06-05 00:55:23 +0530

    Make First API

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-var color = require('colors')
-
-console.log('hello'.bgBlue); // outputs green text
+const http = require('http');
+const data = require('./data')
+http.createServer((req,res)=>{
+res.writeHead(200,{'content-Type': 'application\json' });
+res.write(JSON.stringify(data))
+res.end()
+}).listen(4000)
