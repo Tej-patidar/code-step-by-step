1a614218ad27b9efe273652fdee4cf04d01208da
Tarun <tarunpatidar742@gmail.com>
2023-06-04 23:10:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require('./app')
+const fs = require('fs')
+
+// console.log(app.z())
+
+// const arr = [1,2,3,4,5,6];
+// const filteredvalue = arr.filter((vall)=>{
+//     return vall <= 4;
+// })
+// console.log(filteredvalue)
+
+
+// fs.writeFileSync('hello.txt',"This is the NewTxt file");
+console.log(__filename)
