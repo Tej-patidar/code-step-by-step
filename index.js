d9e369625c472e937df054daf971a9e203315038
Tarun <tarunpatidar742@gmail.com>
2023-06-05 19:30:39 +0530

    Setup of ejs page

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
- const publicPath = path.join(__dirname,'public')
- 
-//  app.use(express.static(publicPath))
+const publicPath = path.join(__dirname, "public");
+app.set("view engine",'ejs' )
+app.get("", (_, res) => {
+  res.sendFile(`${publicPath}/index.html`);
+});
 
-app.get('',(_,res)=>{
-     res.sendFile(`${publicPath}/index.html`);
-})
+app.get("/profile", (_, res) => {
+     let user = {
+          Name: 'Tej Patidar',
+          Fname: 's.l. patidar',
+          Mnane: 'sangeeta patiar',
+          email: 'tej@gmail.com',
+     }
+     res.render('profile',{user})
+   });
 
-app.get('/about',(_,res)=>{
-     res.sendFile(`${publicPath}/about.html`);
-})
+app.get("/about", (_, res) => {
+  res.sendFile(`${publicPath}/about.html`);
+});
 
-app.get('*',(_,res)=>{
-     res.sendFile(`${publicPath}/Nopage.html`);
-})
+app.get("*", (_, res) => {
+  res.sendFile(`${publicPath}/Nopage.html`);
+});
 
 app.listen(4000);
