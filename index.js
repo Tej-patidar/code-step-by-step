59a48289c7e0ce3540c4e9eaa0892264f20342ac
Tarun <tarunpatidar742@gmail.com>
2023-06-05 23:04:14 +0530

    Router-level middleware

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const app = express();
-
-const reqFilter = (req, res, next) => {
-if(!req.query.age){
-     res.send('please Provide Age !!')
-}else if(req.query.age < 18){
-res.send('You can not access these Page !!')
-}else{
-
-}
-  next()
-};
-app.use(reqFilter)
+const reqFilter = require('./middleware');
+const router = express.Router();
+router.use(reqFilter)
+// app.use(reqFilter)
 app.get("/", (req, res) => {
   res.send("hello bhaii This is the HOmepage !!!");
 });
@@ -19,4 +11,13 @@ app.get("/users", (req, res) => {
   res.send("hello bhaii This is the Users Page !!!!");
 });
 
+
+app.get("/about", (req, res) => {
+     res.send("hello bhaii This is the about Page !!!!");
+   });
+
+   app.get("/contact",reqFilter, (req, res) => {
+     res.send("hello bhaii This is the contact Page !!!!");
+   });
+   
 app.listen(4000);
