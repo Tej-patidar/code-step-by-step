ee5ed03c503d768de79ab7c390f161bb90a8ac5a
Tarun <tarunpatidar742@gmail.com>
2023-06-05 18:34:49 +0530

    path to static files

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+ const publicPath = path.join(__dirname,'public')
  
-
-app.get('/',(req,res)=>{
-     console.log("these is the data given by the Browser >>>>", req.query.name)
-     res.send("<h1>Hello These is the Home Page </h1>")
-})
-
-app.get('/about',(req,res)=>{
-     res.send(`
-     <input type=text  placeholder="Type Name" value=" ${req.query.name}"/>
-     `)
-})
-
-
-app.get('/help',(req,res)=>{
-     res.send("These is a help Homepage")
-})
-
+ app.use(express.static(publicPath))
 
 
 app.listen(4000);
