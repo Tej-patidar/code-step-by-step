ab01d0146e9dd31021860598ddb159dd9ffce006
Tarun <tarunpatidar742@gmail.com>
2023-06-05 22:39:47 +0530

    Application level middleware

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
 const express = require("express");
-const path = require("path");
 const app = express();
-const publicPath = path.join(__dirname, "public");
-app.set("view engine",'ejs' )
-app.get("", (_, res) => {
-  res.sendFile(`${publicPath}/index.html`);
+
+const reqFilter = (req, res, next) => {
+if(!req.query.age){
+     res.send('please Provide Age !!')
+}else if(req.query.age < 18){
+res.send('You can not access these Page !!')
+}else{
+
+}
+  next()
+};
+app.use(reqFilter)
+app.get("/", (req, res) => {
+  res.send("hello bhaii This is the HOmepage !!!");
 });
-
-app.get("/profile", (_, res) => {
-     let user = {
-          Name: 'Tej Patidar',
-          Fname: 's.l. patidar',
-          Mnane: 'sangeeta patiar',
-          email: 'tej@gmail.com',
-          skills:['html','css','bootstrap','javascript','Nodejs','MongoDB']
-     }
-     res.render('profile',{user})
-   });
-
-   app.get('/login',(_,res) => {
-     res.render('login')
-   })
-
-app.get("/about", (_, res) => {
-  res.sendFile(`${publicPath}/about.html`);
-});
-
-app.get("*", (_, res) => {
-  res.sendFile(`${publicPath}/Nopage.html`);
+app.get("/users", (req, res) => {
+  res.send("hello bhaii This is the Users Page !!!!");
 });
 
 app.listen(4000);
