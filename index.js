031d25e9dde516f6866e01192c6c1f1bd095aaef
Tarun <tarunpatidar742@gmail.com>
2023-06-05 19:04:44 +0530

    remove etnName from url

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const path = require('path');
 const app = express();
  const publicPath = path.join(__dirname,'public')
  
- app.use(express.static(publicPath))
+//  app.use(express.static(publicPath))
 
+app.get('',(_,res)=>{
+     res.sendFile(`${publicPath}/index.html`);
+})
+
+app.get('/about',(_,res)=>{
+     res.sendFile(`${publicPath}/about.html`);
+})
+
+app.get('*',(_,res)=>{
+     res.sendFile(`${publicPath}/Nopage.html`);
+})
 
 app.listen(4000);
