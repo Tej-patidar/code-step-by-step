5f3f0813975684eb7e5d5e4ba93d816849d8c1de
Tarun <tarunpatidar742@gmail.com>
2023-06-06 07:54:12 +0530

    separe file for mongodb

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
-// const { MongoClient } = require('mongodb');
-// const url = 'mongodb://localhost:27017';
-
-// const client = new MongoClient(url);
-// const database = 'E-com';
-
-// async function getData()
-// {
-//     let result =await  client.connect()
-//     let db = result.db(database);
-//     let collection = db.collection('products');
-//     let response = await collection.find({}).toArray();
-//     console.log(response);
-// }
-
-// getData();
+const dbConnect = require('./mongodb')
 
 
-
-// ........................... 
-
-
-const {MongoClient} = require('mongodb')
-const url= 'mongodb://127.0.0.1:27017/';
-const databaseName='E-com'
-const client= new MongoClient(url);
-
-async function getData()
-{
-    let result = await client.connect();
-    db= result.db(databaseName);
-    collection = db.collection('products');
-    let data = await collection.find({}).toArray();
-    console.log(data)
+// dbConnect().then((res)=>{
+//     res.find().toArray()
+// .then((data)=>{
+//     console.log(data);
+// })
+// })
 
 
-
+const main =async ()=>{
+    let data = await dbConnect();
+  data = await  data.find().toArray();
+    console.log(data);
 }
-
-getData();
+main();
