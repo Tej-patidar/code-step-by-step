77d359717a6f10e67fa420525b13f90898bb00e7
Tarun <tarunpatidar742@gmail.com>
2023-06-06 07:09:30 +0530

    connect to mongodb

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-const express = require("express");
-const app = express();
-const reqFilter = require('./middleware');
-const router = express.Router();
-router.use(reqFilter)
-// app.use(reqFilter)
-app.get("/", (req, res) => {
-  res.send("hello bhaii This is the HOmepage !!!");
-});
-app.get("/users", (req, res) => {
-  res.send("hello bhaii This is the Users Page !!!!");
-});
+// const { MongoClient } = require('mongodb');
+// const url = 'mongodb://localhost:27017';
+
+// const client = new MongoClient(url);
+// const database = 'E-com';
+
+// async function getData()
+// {
+//     let result =await  client.connect()
+//     let db = result.db(database);
+//     let collection = db.collection('products');
+//     let response = await collection.find({}).toArray();
+//     console.log(response);
+// }
+
+// getData();
 
 
-app.get("/about", (req, res) => {
-     res.send("hello bhaii This is the about Page !!!!");
-   });
 
-   app.get("/contact",reqFilter, (req, res) => {
-     res.send("hello bhaii This is the contact Page !!!!");
-   });
-   
-app.listen(4000);
+// ........................... 
+
+
+const {MongoClient} = require('mongodb')
+const url= 'mongodb://127.0.0.1:27017/';
+const databaseName='E-com'
+const client= new MongoClient(url);
+
+async function getData()
+{
+    let result = await client.connect();
+    db= result.db(databaseName);
+    collection = db.collection('products');
+    let data = await collection.find({}).toArray();
+    console.log(data)
+
+
+
+}
+
+getData();
