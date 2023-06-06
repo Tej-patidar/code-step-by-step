56aa04693fa979f98b3d97ce7851017c2dab4a5d
Tarun <tarunpatidar742@gmail.com>
2023-06-07 00:22:41 +0530

    search filter in the api with regex

--- index.js
+++ index.js
@@ -2,40 +2,19 @@ const express = require("express");
 require("./config");
 const product = require("./product");
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-// For creating Data 
-app.post("/create", async(req, res) => {
-  let data = new product(req.body) ;
-  let result = await data.save();
-  console.log(result)
-  // console.log(req.body);
-  res.send("done!!");
+// For search Data
+app.get("/search/:key", async (req, res) => {
+  let data = await product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { brand: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+    ],
+  });
+  console.log(data);
+  res.send(data);
 });
-
-// For get the List of the data 
-app.get('/list',async(req,res)=>{
-  let data =await product.find();
-  console.log(data)
-  res.send('list..!!!')
-})
-
-
-// for deleting data 
-
-app.delete('/delete/:_id',async(req,res)=>{
-  const data = await product.deleteOne(req.params);
-  console.log(req.params)
-  res.send(data);
-})
-
-// for update data 
-
-app.put('/update/:_id',async(req,res)=>{
-  const data = await product.updateOne(req.params,{$set:req.body});
-  res.send(data);
-})
-
-
 
 app.listen(4000);
